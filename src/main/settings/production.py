fb19a549475776673a9c0fc3caf16840cc4601b4
paulwinex <paulwinex@gmail.com>
2023-09-10 22:52:09 +0300

    fix production settings

--- src/main/settings/production.py
+++ src/main/settings/production.py
@@ -1,14 +1,14 @@
 from .base import *
 
-DEBUG = True
+DEBUG = False
 SECRET_KEY = 'sdfo4ifILF*^E(HFKJSHV*(EFY$#IfnfklEcoieh*&YEVIL'
 ALLOWED_HOSTS = ["*"]
 CORS_ORIGIN_ALLOW_ALL = False
-# CSRF_TRUSTED_ORIGINS = ['*']
-CORS_ORIGIN_WHITELIST = ['http://localhost:8000']
-CSRF_TRUSTED_ORIGINS = ["http://localhost:8000"]
-CSRF_ALLOWED_ORIGINS = ["http://localhost:8000"]
-CORS_ORIGINS_WHITELIST = ["http://localhost:8000"]
+ORIGIN = os.getenv('ORIGIN', 'http://localhost:8000')
+CORS_ORIGIN_WHITELIST = [ORIGIN]
+CSRF_TRUSTED_ORIGINS = [ORIGIN]
+CSRF_ALLOWED_ORIGINS = [ORIGIN]
+CORS_ORIGINS_WHITELIST = [ORIGIN]
 
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 try:
